188959906e92b95cd4c1b534cc05133386efd6f1
neetoons <neet.toons@gmail.com>
2024-12-10 20:27:30 -0400

    temporal fix: warn when broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,17 +10,18 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://neetoons.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/neetoons-blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'neetoons', // Usually your GitHub org/user name.
+  projectName: 'neetoons-blog', // Usually your repo name.
+  deploymentBranch: 'deploy',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
